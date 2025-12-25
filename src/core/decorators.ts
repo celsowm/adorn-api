f0d157cbe533815a30a4c5cb18d2c7bb8a67a198
celsowm <celsowm@gmail.com>
2025-12-25 05:28:57 -0300

    sss

--- src/core/decorators.ts
+++ src/core/decorators.ts
@@ -10,9 +10,9 @@ export type ControllerOptions = {
   tags?: string[];
 };
 
-export function Controller(basePath: string, opts: ControllerOptions = {}): <T extends Function>(value: T, context: ClassDecoratorContext) => T {
-  return function <T extends Function>(value: T, context: ClassDecoratorContext) {
-    writeControllerMeta(context.metadata, { basePath, tags: opts.tags ?? [] });
+export function Controller(basePath: string, opts: ControllerOptions = {}): <T extends Function>(value: T, _context: ClassDecoratorContext) => T {
+  return function <T extends Function>(value: T, _context: ClassDecoratorContext) {
+    writeControllerMeta(_context.metadata, { basePath, tags: opts.tags ?? [] });
     return value;
   };
 }
@@ -26,19 +26,19 @@ export type RouteOptions = {
   guards?: Guard[];
 };
 
-function http(method: string, path: string, opts: RouteOptions): (value: Function, context: ClassMethodDecoratorContext) => void {
-  return function (_value: Function, context: ClassMethodDecoratorContext) {
-    if (context.kind !== 'method')
+function http(method: string, path: string, opts: RouteOptions): (value: Function, _context: ClassMethodDecoratorContext) => void {
+  return function (value: Function, _context: ClassMethodDecoratorContext) {
+    if (_context.kind !== 'method')
       throw new RouteConfigError(`@${method} can only decorate methods`);
 
-    if (typeof context.name !== 'string') {
+    if (typeof _context.name !== 'string') {
       throw new RouteConfigError(`Decorated method name must be a string (got symbol)`);
     }
 
     const routeStub: RouteStub = {
       method,
       path,
-      handlerName: context.name,
+      handlerName: _context.name,
       schemas: {
         params: opts.params,
         query: opts.query,
@@ -52,14 +52,14 @@ function http(method: string, path: string, opts: RouteOptions): (value: Functio
       routeStub.includePolicy = opts.includePolicy;
     }
 
-    pushRouteStub(context.metadata, routeStub);
+    pushRouteStub(_context.metadata, routeStub);
 
     return;
   };
 }
 
-export const Get = (path: string, opts: RouteOptions): (value: Function, context: ClassMethodDecoratorContext) => void => http('GET', path, opts);
-export const Post = (path: string, opts: RouteOptions): (value: Function, context: ClassMethodDecoratorContext) => void => http('POST', path, opts);
-export const Put = (path: string, opts: RouteOptions): (value: Function, context: ClassMethodDecoratorContext) => void => http('PUT', path, opts);
-export const Patch = (path: string, opts: RouteOptions): (value: Function, context: ClassMethodDecoratorContext) => void => http('PATCH', path, opts);
-export const Delete = (path: string, opts: RouteOptions): (value: Function, context: ClassMethodDecoratorContext) => void => http('DELETE', path, opts);
+export const Get = (path: string, opts: RouteOptions): (value: Function, _context: ClassMethodDecoratorContext) => void => http('GET', path, opts);
+export const Post = (path: string, opts: RouteOptions): (value: Function, _context: ClassMethodDecoratorContext) => void => http('POST', path, opts);
+export const Put = (path: string, opts: RouteOptions): (value: Function, _context: ClassMethodDecoratorContext) => void => http('PUT', path, opts);
+export const Patch = (path: string, opts: RouteOptions): (value: Function, _context: ClassMethodDecoratorContext) => void => http('PATCH', path, opts);
+export const Delete = (path: string, opts: RouteOptions): (value: Function, _context: ClassMethodDecoratorContext) => void => http('DELETE', path, opts);
