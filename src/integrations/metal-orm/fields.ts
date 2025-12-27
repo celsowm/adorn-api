7b49428fdc261dee1cc1fd4b9dd2255215753141
celsowm <celsowm@gmail.com>
2025-12-27 13:39:01 -0300

    safsdf

--- src/integrations/metal-orm/fields.ts
+++
@@ -1,8 +0,0 @@
-export function defineEntityFields<T>() {
-  return <K extends readonly (keyof T)[]>(...fields: K): K => fields;
-}
-
-export function fieldsOf<T>() {
-  return <K extends readonly (keyof T)[]>(...fields: K): K => fields;
-}
-
--- tests/setup/vitest.setup.ts
+++
@@ -1,2 +0,0 @@
-import { ensureDecoratorMetadata } from '../../src/runtime/metadataPolyfill.js';
-ensureDecoratorMetadata();
